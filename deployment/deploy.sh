4b04320c1020e386896634484512252b4e1b54ad
Ha Long <longvn00@gmail.com>
2021-06-24 21:04:17 +0700

    move config to deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../config.sh
+source ./config.sh
 
 # VARIABLES DEFINED
 ENV=$1
--- deployment/upload.sh
+++ deployment/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ../config.sh
+source ./config.sh
 
 aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
